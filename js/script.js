567ac4858c8d361e5c93fba796741a36cf89abf8
JValbjorn <julievalbjorn@live.dk>
2025-01-27 09:31:44 +0100

    done

---
+++ js/script.js
@@ -0,0 +1,25 @@
+`use strict`; 
+
+let navn = `Julie`;
+let age = 21; 
+console.log(`Hi ${navn}, you are ${age} years old!`);
+
+ navn = `Malene`;
+age = 20;
+console.log(`Hi ${navn}, you are ${age} years old!`);
+
+// Link to the external JavaScript file placed in the `js` folder:
+// ```html
+// <script src="js/script.js"></script>
+// ```
+// 2. Ensure "use strict" is at the top of your `script.js` file.
+
+// 3. Create a variable for your `[name]` and a variable for your `[age]` using `let`.
+
+// 4. Use `console.log` to display the message:
+
+// ```html
+// "Hi [Name], you are [Age] years old!"
+// ```
+
+// 5. Reassign the `[name]` and `[age]` variables to new values, and display the updated message using `console.log`.
